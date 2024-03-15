b443e4d729d07a065f9ff0a58d00b123fb73db09
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-03-16 01:21:13 +0200

    added time to ride object

--- models/ride.js
+++ models/ride.js
@@ -5,9 +5,10 @@ const url = process.env.MONGODB_URI
 mongoose.connect(url)
 
 const Ride = mongoose.model('Ride', {
-    destination: String,
-    pickup: String,
-    latestArrivalTime: String
+    destination: JSON,
+    pickup: JSON,
+    time: String
+    
 })
     
 module.exports = Ride
